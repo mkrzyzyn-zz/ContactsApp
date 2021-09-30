f95b28d4363950235a05bb825cf93dfd1444ce27
mkrzyzyn <maciej.krzyzynski@gmail.com>
2021-09-30 14:32:13 +0200

    Remove duplicate getters and setters in subclasess.

--- Contacts/task/src/contacts/Person.java
+++ Contacts/task/src/contacts/Person.java
@@ -14,32 +14,6 @@ public class Person extends Contact {
         return name + " " + surname;
     }
 
-    ;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     private Person(Builder builder) {
         this.name = builder.name;
         this.surname = builder.surname;
